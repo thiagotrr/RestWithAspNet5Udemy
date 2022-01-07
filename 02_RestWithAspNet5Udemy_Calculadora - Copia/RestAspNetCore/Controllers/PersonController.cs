217ffeae81a8ebcba5d2edecc3a2c560ffd5dfae
thiagotrr <thiago.trrodrigues@gmail.com>
2022-01-06 21:44:09 -0300

    Conectando com Banco de Dados

---
+++ 02_RestWithAspNet5Udemy_Calculadora - Copia/RestAspNetCore/Controllers/PersonController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestAspNetCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAspNetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Input inválido!");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Input inválido!");
+        }
+
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public IActionResult Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+            return BadRequest("Input inválido!");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(secondNumber) == 0)
+                {
+                    return BadRequest("Impossível dividir por zero!");
+                }
+
+                var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(div.ToString());
+
+            }
+            return BadRequest("Input inválido!");
+        }
+
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var avg = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(avg.ToString());
+            }
+            return BadRequest("Input inválido!");
+        }
+
+        [HttpGet("raizq/{firstNumber}")]
+        public IActionResult RaizQ(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                if (ConvertToDecimal(firstNumber) < 0)
+                {
+                    return BadRequest("Não é possível calcular raiz de número negativo!");
+                }
+                var sqtr = System.Math.Sqrt(Convert.ToDouble(firstNumber)); // ou (double) ConvertToDecimal(firstNumber)
+                return Ok(sqtr.ToString());
+            }
+
+            return BadRequest("Input inválido!");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            
+            return isNumber;
+            
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ 02_RestWithAspNet5Udemy_Calculadora - Copia/RestAspNetCore/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,75 @@
+﻿using RestAspNetCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RestAspNetCore.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+
+        private volatile int count;
+        public Person Create(Person person)
+        {
+            // Acesso a base!
+
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            // Por ora, nada...
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Thiago",
+                LastName = "Rodrigues",
+                Address = "Cataguases, MG, BR",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            // Ações de DB
+
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + i,
+                LastName = "Person LastName" + i,
+                Address = "Adress" + i,
+                Gender = "Masculino"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
